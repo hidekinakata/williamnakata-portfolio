92bde08b39f77880b02b4d9a8b009432c3968854
William Hideki Nakata <whnakata@gmail.com>
2026-03-03 01:01:58 -0300

    feat(layout): enhance theming and style management
    Switches to dynamic font loading via CDN and sets language to Brazilian Portuguese to improve localization. Implements theme detection based on cookies or system preferences for flexible dark mode handling. Enhances global styles for smoother color transitions and updates dark variant selectors for improved compatibility.
    
    Improves accessibility, performance, and theming flexibility.
    
    Fixes #301

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,48 @@
 import type { Metadata } from "next";
-import {Bodoni_Moda, Geist, Geist_Mono, Google_Sans_Code, Zalando_Sans_SemiExpanded} from "next/font/google";
 import "./globals.css";
 
-const fontSans = Zalando_Sans_SemiExpanded({
-  variable: "--font-sans",
-  subsets: ["latin"],
-})
-
-const fontSerif = Bodoni_Moda({
-  variable: "--font-serif",
-  subsets: ["latin"],
-})
-
-const fontMono = Google_Sans_Code({
-  variable: "--font-mono",
-  subsets: ["latin"],
-})
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "William Nakata Portfolio",
   description: "My personal portfolio builded with Next.js",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme")?.value || "system";
+  const themeprod = theme === "dark" ? "dark" : "";
   return (
-    <html lang="en">
-      <body
-        className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-BR" className={themeprod} suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function() {
+              const theme = document.cookie.split('; ').find(row => row.startsWith('theme='))?.split('=')[1] || 'system';
+              if (theme === 'dark' || (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+              } else {
+                document.documentElement.classList.remove('dark');
+              }
+            })()
+          `,
+          }}
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Google+Sans+Code:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&family=Zalando+Sans+Expanded:ital,wght@0,200..900;1,200..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
