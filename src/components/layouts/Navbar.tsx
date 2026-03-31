28dc72633d80d91c40f224afa99de5ac73d5f9dd
William Hideki Nakata <whnakata@gmail.com>
2026-03-31 14:45:38 -0300

    feat(ui): enhance Navbar with responsive design and visual features
    Reimplement the Navbar component to include a responsive design suitable for various screen sizes. Introduce new features like a toggleable burger menu for mobile, visual GradualBlur effects, and interactive nav items with animations and color transitions.

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -2,13 +2,14 @@
 
 import React, { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
-import { useAppContext } from "@/context/AppContext";
+import GradualBlur from "@/components/GradualBlur";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
-  // const { theme, toggleTheme, language, toggleLanguage } = useAppContext();
   const t = useTranslations("Menu");
   const [isVisible, setIsVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -26,36 +27,94 @@ const Navbar = () => {
     };
 
     window.addEventListener("scroll", handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
+
+  const navItems = [t("About"), t("Experience"), t("Projects")];
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 transition-transform duration-300 bg-linear-to-b from-black/50 via-black/10 to-black/0 ${isVisible ? "translate-y-0" : "-translate-y-full"}`}
+      className={cn(
+        "fixed top-0 left-0 right-0 z-50",
+        "grid grid-cols-[1fr_auto_1fr] items-center",
+        "h-20 lg:h-28 px-6 lg:px-12",
+        "transition-transform duration-300",
+        "bg-linear-to-b from-black/50 via-black/10 to-black/0",
+        isVisible ? "translate-y-0" : "-translate-y-full",
+      )}
     >
-      <div className={"py-8 px-8 flex justify-between items-start"}>
-        <h1 className={"uppercase flex flex-col *:leading-none"}>
-          <span className={"text-xl font-thin"}>William</span>
-          <span className={"text-3xl font-extrabold"}>Nakata</span>
-        </h1>
+      <GradualBlur
+        target="parent"
+        position="top"
+        height="100%"
+        strength={0.5}
+        divCount={7}
+        curve="bezier"
+        exponential
+        opacity={1}
+        className={"-z-1!"}
+      />
 
-        <nav>
-          <ul
-            className={
-              "flex flex-col justify-center items-end gap-2 text-sm uppercase"
-            }
-          >
-            <li>{t("Home")}</li>
-            <li>{t("About")}</li>
-            <li>{t("Experience")}</li>
-            <li>{t("Projects")}</li>
-            <li>{t("Contact")}</li>
-            <li>{t("Blog")}</li>
-          </ul>
-        </nav>
+      {/* Nav desktop/tablet */}
+      <nav className="hidden lg:block ">
+        <ul className="flex justify-start items-center gap-5 text-xs uppercase">
+          {navItems.map((item, index) => (
+            <li
+              key={item}
+              className="relative cursor-pointer text-neutral-400 hover:text-white transition-colors tracking-wider"
+            >
+              {item}
+              <span
+                className={
+                  "absolute top-1 right-0 -translate-y-full translate-x-full text-[0.51rem] text-violet-500"
+                }
+              >
+                0{index}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* Logo */}
+      <h1 className="uppercase flex flex-col items-center *:leading-none col-start-2">
+        <span className="text-base lg:text-xl font-thin text-white/60 mr-2 lg:mr-3">
+          William
+        </span>
+        <span className="text-xl lg:text-3xl font-extrabold text-white">
+          Nakata
+        </span>
+      </h1>
+
+      <div className="lg:hidden flex justify-end items-center">
+        {/*buger menu*/}
+        <button
+          className={
+            "flex flex-col itens-center justify-center gap-1 p-2 aspect-square cursor-pointer"
+          }
+        >
+          <span
+            className={"block h-0.5 w-3.5 bg-white rounded place-self-end"}
+          />
+          <span className={"block h-0.5 w-6 bg-white rounded"} />
+          <span className={"block h-0.5 w-3.5 bg-white rounded"} />
+        </button>
+      </div>
+
+      <div className="hidden lg:block">
+        <div className={"flex justify-end items-center gap-2"}>
+          <div className="w-fit flex items-center py-2 px-4 gap-2 bg-lime-400/8 border border-solid border-lime-500/50 antialiased text-xs/4">
+            <div className="tracking-[0.2em] uppercase inline-block text-lime-400 font-mono font-medium shrink-0 text-[10px]/3">
+              {t("Blog")}
+            </div>
+          </div>
+
+          <div className="w-fit flex items-center py-2 px-4 gap-2 bg-violet-400/10 border border-solid border-violet-500/50 antialiased text-xs/4">
+            <div className="tracking-[0.2em] uppercase inline-block text-violet-400 font-mono font-medium shrink-0 text-[10px]/3">
+              Download CV
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
